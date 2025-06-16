acd009947639510d75e9d8633290a61001fe2d8d
Muhammad <mitcao.000@gmail.com>
2025-06-16 10:04:31 +0300

    create NextJS app

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const about = () => {
+	return (
+		<>
+			<Head>
+				<title>About Page</title>
+			</Head>
+			<div>About Page</div>
+		</>
+	)
+}
+
+export default about
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const IndexPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Sait App</title>
+			</Head>
+			<div>IndexPage</div>
+		</>
+	)
+}
+
+export default IndexPage
