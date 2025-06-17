5073df8a1af42bf4da2ed55beed1668f5a047d61
Muhammad <mitcao.000@gmail.com>
2025-06-17 11:10:43 +0300

    Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@mui/material'
 import Head from 'next/head'
+import { Hero } from 'src/components'
 import Layout from 'src/layout/layout'
 
 const IndexPage = () => {
 	return (
 		<Layout>
-			<Button></Button>
+			<Hero />
 		</Layout>
 	)
 }
